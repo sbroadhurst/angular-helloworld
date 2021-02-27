28c1a5647af171c27b4402c358d3b894f9d84ccc
sbroadhurst <stepbro31@gmail.com>
2021-02-27 15:51:40 -0500

    change route for form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { WeatherComponent } from './weather/weather.component';
 import { ImagePageComponent } from './image-page/image-page.component';
+import { FormComponentComponent } from './form-component/form-component.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'form', component: FormComponentComponent },
   { path: 'weather', component: WeatherComponent },
   { path: 'images', component: ImagePageComponent },
 ];
